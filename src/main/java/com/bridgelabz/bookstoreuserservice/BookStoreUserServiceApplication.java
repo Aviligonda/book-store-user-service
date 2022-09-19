0aa1a4cf934038a6ad92e7f941b5435393483def
aviligonda <aviligondasreenivasulu6086@gmail.com>
2022-09-19 09:43:18 +0530

    Create BookStore User Service Project

---
+++ src/main/java/com/bridgelabz/bookstoreuserservice/BookStoreUserServiceApplication.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.bookstoreuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class BookStoreUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookStoreUserServiceApplication.class, args);
+    }
+
+}
